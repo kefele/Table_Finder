7111371f4eb448bbc240d701c63c8e54591cc18e
Jonathon Branham <kefele@aim.com>
2018-07-31 06:01:27 -0500

    added more Jquery

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,36 @@
 'use strict'
-$(document).ready(() =>{
+$(document).ready(() => {
     let table;
 
-    $(".available").on("click", (e)=>{
-        table = $(e.target)
-        $("form").show();
-        
-    })
+
+    function setupPage(){
+        $("form").hide();
+
+    };
+
+    function registerEventHandlers() {
+        // register events handlers for mouse enter and leave each letter. Add
+        // a "hover" class when the mouse is over the letter.
+        $("body").on("mouseenter mouseleave", ".table", (e) => {
+            $(e.target).toggleClass("hover");
+        });
+
+        $(".available").on("click", (e) => {
+            table = $(e.target)
+            $("form").show();
+
+        });
 
 
-$("button").click(function(){
+        $("button").click(function () {
 
-    $("form").hide();
-    table.removeClass("available").addClass("reserved");
-})
+            $("form").hide();
+            table.removeClass("available").addClass("reserved");
+        });
 
-$(".exit").click(function(){
-    $("form").hide();
-})
+        $(".exit").click(function () {
+            $("form").hide();
+        });
 
-})
+
+    }
